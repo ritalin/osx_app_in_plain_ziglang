c787bac5c3396d9ef974a9cab6bdf8f15e27d436
Ritalin <kazu.ktz.tamura@gmail.com>
2022-12-29 18:31:06 +0900

    Send message

--- Studying.md
+++ Studying.md
@@ -60,3 +60,19 @@ zig build --sysroot $(xcrun --sdk macosx --show-sdk-path)
 
 see also: https://github.com/kubkon/zig-ios-example
 
+## Send message
+
+`objc_msgSend` is varargs function.
+`@ptrCast` and `@call` could be used to resolve.
+
+```ziglang
+fn msgSend_1(self: objc.Class, selector: objc.SEL) objc.id {
+    const FnType = fn (objc.Class, objc.SEL) callconv(.C) objc.id;
+
+    var func = @ptrCast(FnType, objc.objc_msgSend);
+
+    return @call(.{}, func, .{ self, selector });
+}
+```
+
+see also: https://github.com/hazeycode/zig-objcrt/blob/main/src/message.zig
--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,25 @@
 const std = @import("std");
-const objc = @cImport(@cInclude("objc/objc.h"));
+const objc = @cImport({
+    @cInclude("objc/objc.h");
+    @cInclude("objc/message.h");
+});
+
+fn msgSend_1(self: objc.Class, selector: objc.SEL) objc.id {
+    const FnType = fn (objc.Class, objc.SEL) callconv(.C) objc.id;
+
+    var func = @ptrCast(FnType, objc.objc_msgSend);
+
+    return @call(.{}, func, .{ self, selector });
+}
 
 pub fn main() anyerror!void {
     var allocSEL: objc.SEL = objc.sel_registerName("alloc");
+    // var initSEL: objc.SEL = objc.sel_registerName("init");
+
+    var appClass: objc.Class = objc.objc_getClass("NSApplication");
+    var sharedAppSEL: objc.SEL = objc.sel_registerName("sharedApplication");
+
+    _ = msgSend_1(appClass, sharedAppSEL);
 
     std.debug.assert(allocSEL != null);
     std.debug.print("OK\n", .{});
